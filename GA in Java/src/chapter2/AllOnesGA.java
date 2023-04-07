462a5bd2df2eadd39e153b123ec5620af8e236cf
Samuel Herrmann <samuelherrmann@gmx.net>
2023-04-07 22:41:51 +0200

    initialize Population, fix ArrayOutOfBounds in Population constructor

--- GA in Java/src/chapter2/AllOnesGA.java
+++ GA in Java/src/chapter2/AllOnesGA.java
@@ -4,6 +4,6 @@ public class AllOnesGA {
     public static void main(String[] args) {
         // Create GA object
         GeneticAlgorithm GA = new GeneticAlgorithm(100, 0.01,0.95,0);
-        
+        Population population = GA.initPopulation(50);
     }
 }
