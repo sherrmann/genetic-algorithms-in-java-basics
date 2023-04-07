55f7b28dc70063b73efc4cf3af7f686403295006
Samuel Herrmann <samuelherrmann@gmx.net>
2023-04-07 23:16:06 +0200

    add generation counter, repeatedly evaluate population until terminationCondition is met

--- GA in Java/src/chapter2/AllOnesGA.java
+++ GA in Java/src/chapter2/AllOnesGA.java
@@ -3,7 +3,17 @@ package chapter2;
 public class AllOnesGA {
     public static void main(String[] args) {
         // Create GA object
-        GeneticAlgorithm GA = new GeneticAlgorithm(100, 0.01,0.95,0);
-        Population population = GA.initPopulation(50);
+        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01,0.95,0);
+        Population population = ga.initPopulation(50);
+        int generation = 0;
+        ga.evalPopulation(population);
+
+        while(!ga.isTerminationConditionMet(population)){
+            System.out.println("Best solution: " + population.getFittest(0).toString());
+            ga.evalPopulation(population);
+            generation++;
+        }
+        System.out.println("Found solution in " + generation + " generations");
+        System.out.println("Best solution: " + population.getFittest(0).toString());
     }
 }
